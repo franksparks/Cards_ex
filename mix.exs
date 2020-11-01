a25065b6880fed12215647cfc9a5be42e5a17c46
franksparks <ferranbals@gmail.com>
2020-11-01 12:23:16 +0100

    1.- Update GitHub with local repo

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule Cards.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :cards,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+
+      # Docs
+       name: "Cards",
+       source_url: "https://github.com/USER/PROJECT",
+       homepage_url: "http://YOUR_PROJECT_HOMEPAGE",
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dep_from_hexpm, "~> 0.3.0"},
+      {:ex_doc, "~> 0.23"}
+    ]
+  end
+end
